62245e9114c744280d1faa0a9d7a005a7ecbd133
Nichols A. Romero <nick.romero@amd.com>
2025-11-22 00:31:14 +0000

    Update Triton build script.

--- triton/build.sh
+++ triton/build.sh
@@ -2,4 +2,5 @@
 pip uninstall pytorch-triton-rocm
 pip uninstall triton -y
 pip install -r python/requirements.txt
-pip install .
+# pip install -e . # triton 3.4 and later
+pip install -e python # triton 3.3
