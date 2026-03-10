5635edbdc5ca6d8cff642dab483e27d27ca25708
Nichols A. Romero <nick.romero@amd.com>
2026-03-10 20:34:58 +0000

    Minor clean up.

--- pytorch/build.sh
+++ pytorch/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# Optional if you have a pre-built AOTriton, otherwise, default behavior is too download it.
+# Optional if you have a pre-built AOTriton, otherwise, default behavior is to download it.
 # Build AOTriton is slow, so leave section below alone.
 # AOTRITONBASEDIR=/home/niromero/root/installed_aotriton
 # mkdir -p ${AOTRITONBASEDIR}/0.7b-rocm6.2
@@ -11,9 +11,9 @@
 # next two lines needed when switching between branches as many of the underlying submodules could have changed
 # in significant ways.
 python3 setup.py clean # needed when switching branches
-# git clean . -dfx # some times also needed due to leftoever files due to submodules or hipification
+# git clean . -dfx # some times also needed due to leftoever files due to submodules that have been removed
 git submodule sync --recursive # synchronizes url in .gitmodules, do this recursively
-git submodule foreach --recursive git reset --hard # cleanup in case there is a detached HEAD
+git submodule foreach --recursive git reset --hard # cleanup in case there is a detached HEAD, not fullproof
 git submodule update --init --recursive # clones missing submodules
 
 export MAX_JOBS=128 # use as many CPU cores as possible to build PyTorch
@@ -34,5 +34,6 @@ unset CPLUS_INCLUDE_PATH # needed on some internal AMD docker images.
 # Build system assumes ROCM_PATH=/opt/rocm, set explictly if another version of ROCm is going to be used.
 # export ROCM_PATH=/opt/rocm-6.2.0
 
-# Build a balance subset of PyTorch features
-USE_ROCM=1 REL_WITH_DEB_INFO=1 USE_NNPACK=0 USE_FLASH_ATTENTION=1 USE_MEM_EFF_ATTENTION=1 USE_KINETO=1 USE_FBGEMM=0 USE_MPI=0 USE_MAGMA=1 BUILD_TEST=0 python setup.py install 2>&1 | tee build.log # works
+# Build a subset of PyTorch features, balance of build speed with functionality.
+# don't build MPI support, FBGEMM, or compiled-tests
+USE_ROCM=1 REL_WITH_DEB_INFO=1 USE_FLASH_ATTENTION=1 USE_MEM_EFF_ATTENTION=1 USE_KINETO=1 USE_FBGEMM=0 USE_MPI=0 USE_MAGMA=1 BUILD_TEST=0 python setup.py install 2>&1 | tee build.log # works
